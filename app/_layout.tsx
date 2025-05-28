283d54d4221d18eaaebee70ef7a69d596b0c2254
Nana Mohamadi <adilnana78@gmail.com>
2025-05-28 21:49:59 +0000

    Building the Auth Page UI with React Native Paper && Implementing Route Guards with Expo Router && Handling Sign Up / Sign In Logic && Finalizing UI & Switching Between Auth Modes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const isAuth = false; // Replace with actual authentication logic
+
+  useEffect(() => {
+    if (!isAuth) {
+      setTimeout(() => { // Simulate a delay for demonstration purposes (isReady eviter)
+        router.replace("/auth"); // Redirect to the auth screen after a delay
+      }, 0); 
+    }
+  }, []);
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
   return (
-    <Stack >
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-  </Stack>
-  )
+    <RouteGuard>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </RouteGuard>
+  );
 }
---
+++ app/auth.tsx
@@ -0,0 +1,103 @@
+import { Link } from "expo-router";
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
+
+export default function AuthScreen() {
+  const [isSignup, setIsSignup] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>("");
+
+  const theme = useTheme()
+  // Switch mode
+  const handleSwitchMode = () => {
+    setIsSignup((prev) => !prev);
+  };
+
+  // Handle auth
+  const handleAuth = async () => {
+    if (!email || !password) {
+      setError("Please fill all fields");
+      return;
+    }
+    if(password.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    }
+    setError(null); // Clear any previous errors 
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View style={styles.content}>
+        <Text style={styles.title} variant="headlineMedium">
+          {isSignup ? "Create Account" : "Welcome back"}
+        </Text>
+
+        <TextInput
+          label="Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="exmample@gmail.com"
+          mode="outlined"
+          style={styles.input}
+          onChangeText={setEmail}
+        />
+
+        <TextInput
+          label="Password"
+          autoCapitalize="none"
+          placeholder="Your password..."
+          secureTextEntry
+          mode="outlined"
+          style={styles.input}
+          onChangeText={setPassword}
+        />
+        {error && <Text style={{ color: theme.colors.error }}>{error}</Text>}
+
+        <Button mode="contained" style={styles.button} onPress={handleAuth}>
+          {isSignup ? "Sign Up" : "Sign In"}
+        </Button>
+
+        <Button
+          mode="text"
+          style={styles.switchModeButton}
+          onPress={handleSwitchMode}
+        >
+          {isSignup
+            ? "Already have an account? Sign In"
+            : "Don't have an account? Sign Up"}
+        </Button>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 8,
+  },
+  switchModeButton: {
+    marginTop: 16,
+  },
+});
